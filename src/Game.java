84c424fdbb3ada58d786e1102029341ddeca4869
Wayne Barlow <wbarlow@iinet.net.au>
2017-10-06 12:22:42 +1100

    Code in Game.playRound is changed (again) to fix Bug 1 after reinvestigation due to new understanding of logic that bet should only be subtracted from balance if player has lost the turn.

--- src/Game.java
+++ src/Game.java
@@ -28,8 +28,6 @@ public class Game {
 		if (player == null) throw new IllegalArgumentException("Player cannot be null.");
 		if (pick == null) throw new IllegalArgumentException("Pick cannot be negative.");
 		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
-		
-		player.takeBet(bet);
 		    
 		int matches = 0;
 		for ( Dice d : dice) {
@@ -41,10 +39,19 @@ public class Game {
 		
 		int winnings = matches * bet;
 
-		if (matches > 0) {			
+		if (matches > 0) {
+
 			player.receiveWinnings(winnings);
+
 		}
-        return winnings;		
+
+		else {
+
+			player.takeBet(bet);
+
+		}
+
+		return winnings;
 	}
 	
 }
